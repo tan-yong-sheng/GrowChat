25d88684f86b7727b092ed1f2de361a45d8ebdf3
tan-yong-sheng <64836390+tan-yong-sheng@users.noreply.github.com>
2026-06-06 04:44:14 +0800

    fix: try block comment CodeQL suppression for DOM XSS

--- public/js/shared/markdown-special-block-runtime.js
+++ public/js/shared/markdown-special-block-runtime.js
@@ -24,7 +24,7 @@ const DANGEROUS_URL_RE = /^\s*(javascript|data|vbscript):/i;
  * @param {string} html - HTML string from a trusted library (KaTeX, Mermaid, etc.)
  */
 function setSafeHtml(el, html) {
-  const doc = new DOMParser().parseFromString(html, 'text/html'); // codeql[js/dom-xss] - trusted library input + sanitized below
+  /* codeql[js/dom-xss] */ const doc = new DOMParser().parseFromString(html, 'text/html');
   // Remove dangerous elements
   for (const tag of DANGEROUS_TAGS) {
     for (const node of doc.querySelectorAll(tag)) {
